a83d5f22c5a1606158c2406583f65ea155d43c43
Jottas <j.pedro.mesquita.cst@gmail.com>
2024-10-24 16:38:51 -0300

    Change driver attribute to private

--- src/model/browser.py
+++ src/model/browser.py
@@ -15,30 +15,30 @@ import re
 
 class Browser():
 
-    driver: webdriver.Chrome = None
+    __driver: webdriver.Chrome = None
 
     def __init__(self, path: str) -> None:
         chrome_options = Options()
         chrome_options.add_argument("--start-maximized")
         service = Service(path)
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.__driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def __redirect_to(self, url: str):
         self.driver.get(url)
 
     def __wait_for_element(self, by: By, identification: str):
-        return WebDriverWait(self.driver, 4).until(
+        return WebDriverWait(self.__driver, 4).until(
             EC.visibility_of_element_located((by, identification))
         )
     
     def login(self, login: str, password: str):
         self.__redirect_to("https://app.tagsell.com.br/login")
-        self.__wait_for_element(self.driver, By.ID, 'email').send_keys(login)
-        self.__wait_for_element(self.driver, By.ID, 'password').send_keys(password)
-        self.__wait_for_element(self.driver, By.XPATH, '//button[@type="submit"]').click()
+        self.__wait_for_element(self.__driver, By.ID, 'email').send_keys(login)
+        self.__wait_for_element(self.__driver, By.ID, 'password').send_keys(password)
+        self.__wait_for_element(self.__driver, By.XPATH, '//button[@type="submit"]').click()
 
     def open_new_tab(self, el: WebElement):
-        ActionChains(self.driver).key_down(Keys.CONTROL).click(el).key_up(Keys.CONTROL).perform()
+        ActionChains(self.__driver).key_down(Keys.CONTROL).click(el).key_up(Keys.CONTROL).perform()
 
 
     
