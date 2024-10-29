7cfc5fd3495247a787e9a3e655ea4e275a408497
Jottas <j.pedro.mesquita.cst@gmail.com>
2024-10-29 15:09:26 -0300

    UI main_window layout

--- src/UI/main_window.py
+++ src/UI/main_window.py
@@ -1,14 +1,17 @@
 import customtkinter
 from PIL import Image
 from src.UI.table import Table
+from src.UI.console import Console 
+from src.model.product import Product
+from typing import List
 
 class MainWindow(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, products: List[Product]):
         super().__init__()
         customtkinter.set_appearance_mode("light")
 
         # Configurações da janela
-        self.geometry("1000x811")
+        self.geometry("1000x877")
         self.title("Auto Tag")
         self.resizable(False, False)
 
@@ -25,7 +28,6 @@ class MainWindow(customtkinter.CTk):
         self.h1_title.grid(row=0, column=0, padx=(50, 0), pady=(30, 0), sticky="nw")
 
         # Tabela de produtos
-
         # Título da tabela
         self.table_title = customtkinter.CTkLabel(
             self,
@@ -37,38 +39,15 @@ class MainWindow(customtkinter.CTk):
         self.table_title.grid(row=1, column=0, padx=(50, 0), pady=(50, 0), sticky="w")
 
         # Tabela
-        self.table = Table(master=self, width=650, height=450, corner_radius=0, fg_color="#DDD")
+        self.table = Table(master=self, products=products, width=650, height=450, corner_radius=0, fg_color="#DDD")
         self.table.grid(row=3, column=0, padx=(50, 0), pady=(1, 1), sticky="nsew")
 
 
         # Botões lado direito -----------------------------------------------------------
-        
-        # SMGOI13
-        self.btn_smgoi13_upper_text = customtkinter.CTkLabel(
-            self,
-            text="Data SMG13 [27/10]",
-            text_color="#000",
-            font=("Inter", 12, "bold"),
-        )
-        self.btn_smgoi13_upper_text.grid(row=3, column=2, padx=(80, 0), pady=(1, 1), sticky="ne")
-
         img_excel = customtkinter.CTkImage(
             light_image=Image.open("./src/assets/img/excel_icon.png"),
             size=(29, 29)
         )
-
-        self.btn_smgoi13 = customtkinter.CTkButton(
-            self,
-            text="SMGOI13",
-            text_color="#000",
-            font=("Inter", 12, "bold"),
-            fg_color="#B2B2B2",
-            image=img_excel,
-            width=140,
-            height=45,
-            corner_radius=0
-        )
-        self.btn_smgoi13.grid(row=3, column=2, padx=(80, 0), pady=(25, 1), sticky="ne")
 
         # Produtos
         self.btn_products_upper_text = customtkinter.CTkLabel(
@@ -151,3 +130,114 @@ class MainWindow(customtkinter.CTk):
             corner_radius=0
         )
         self.btn_tagsell.grid(row=3, column=2, padx=(80, 0), pady=(250, 1), sticky="ne")
+
+
+        # Botões de baixo -----------------------------------------------------------------------
+        # Preço
+        img_price = customtkinter.CTkImage(
+            light_image=Image.open("./src/assets/img/price_icon.png"),
+            size=(24, 24)
+        )
+
+        self.btn_price_upper_text = customtkinter.CTkLabel(
+            self,
+            text="Alterar os preços",
+            text_color="#000",
+            fg_color="transparent",
+            font=("Inter", 10, "bold"),
+        )
+        self.btn_price_upper_text.grid(row=4, column=0, padx=(50, 0), pady=(0, 0), sticky="nw")
+
+        self.btn_price = customtkinter.CTkButton(
+            self,
+            text="PREÇO",
+            text_color="#000",
+            font=("Inter", 12, "bold"),
+            fg_color="#B2B2B2",
+            image=img_price,
+            width=140,
+            height=45,
+            corner_radius=0
+        )
+        self.btn_price.grid(row=4, column=0, padx=(50, 0), pady=(25, 0), sticky="w")
+
+        # Imprimir
+        img_print = customtkinter.CTkImage(
+            light_image=Image.open("./src/assets/img/pdf_icon.png"),
+            size=(24, 24)
+        )
+
+        self.btn_print_upper_text = customtkinter.CTkLabel(
+            self,
+            text="Salvar em PDF",
+            text_color="#000",
+            fg_color="transparent",
+            font=("Inter", 10, "bold"),
+        )
+        self.btn_print_upper_text.grid(row=4, column=0, padx=(205, 0), pady=(0, 0), sticky="nw")
+
+        self.btn_print = customtkinter.CTkButton(
+            self,
+            text="IMPRIMIR",
+            text_color="#000",
+            font=("Inter", 12, "bold"),
+            fg_color="#B2B2B2",
+            image=img_print,
+            width=140,
+            height=45,
+            corner_radius=0
+        )
+        self.btn_print.grid(row=4, column=0, padx=(205, 0), pady=(25, 0), sticky="nw")
+
+        # Limpar
+        img_clear = customtkinter.CTkImage(
+            light_image=Image.open("./src/assets/img/clear_icon.png"),
+            size=(24, 24)
+        )
+
+        self.btn_clear_upper_text = customtkinter.CTkLabel(
+            self,
+            text="Limpar a lista",
+            text_color="#000",
+            fg_color="transparent",
+            font=("Inter", 10, "bold"),
+        )
+        self.btn_clear_upper_text.grid(row=4, column=0, padx=(0, 73), pady=(0, 0), sticky="ne")
+
+        self.btn_clear = customtkinter.CTkButton(
+            self,
+            text="LIMPAR",
+            text_color="#000",
+            font=("Inter", 12, "bold"),
+            fg_color="#B2B2B2",
+            image=img_clear,
+            width=140,
+            height=45,
+            corner_radius=0
+        )
+        self.btn_clear.grid(row=4, column=0, padx=(0, 0), pady=(25, 0), sticky="ne")
+
+
+        # Console --------------------------------------------------------------------
+        self.console_upper_text = customtkinter.CTkLabel(
+            self,
+            text="LOG do sistema:",
+            text_color="#000",
+            fg_color="transparent",
+            font=("Inter", 10, "bold"),
+        )
+        self.console_upper_text.grid(row=5, column=0, padx=(50, 0), pady=(20, 0), sticky="nw")
+        
+        self.console = Console(self, corner_radius=0, height=120, fg_color="#DDD")
+        self.console.grid(row=6, column=0, padx=(50, 0), pady=(0, 0), columnspan=12, sticky="we")
+
+
+        # Créditos
+        self.credits = customtkinter.CTkLabel(
+            self,
+            text="Desenvolvido por João Pedro Mesquita | j.pedro.mesquita.cst@gmail.com",
+            text_color="#000",
+            font=("Inter", 10, "bold")
+        )
+        self.credits.grid(row=7, column=0, padx=(50, 0), pady=(20, 0), sticky="nse")
+        
--- src/UI/table.py
+++ src/UI/table.py
@@ -3,36 +3,34 @@ from src.model.product import Product
 from typing import List
 
 class Table(customtkinter.CTkScrollableFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, products: List[Product], **kwargs):
         super().__init__(master, **kwargs)
 
         # Cabeçalho da tabela (COD, Desc, Emb, Preço, Local, Status)
         self.table_header = TableHead(master=self, width=650, height=30, corner_radius=0, fg_color="#000")
         self.table_header.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
 
-        # if len(products) > 0:
-        #     for product in products:
-        #         # Linhas da tabela
-        #         self.table_row = TableRow(
-        #                 master=self,
-        #                 width=650,
-        #                 height=30,
-        #                 corner_radius=0,
-        #                 fg_color="#FFF",
-        #                 code=product.get_code(),
-        #                 desc=product.get_descritpion(),
-        #                 emb=product.get_emb(),
-        #                 price=product.get_price(),
-        #                 status=product.get_status(),
-        #                 local=product.get_local()
-        #             )
-        #         self.table_row.grid(row=1, column=0, padx=0, pady=0, sticky="nsew")
+        if len(products) > 0:
+            for i, product in enumerate(products):
+                # Linhas da tabela
+                self.table_row = TableRow(
+                        master=self,
+                        width=650,
+                        height=30,
+                        corner_radius=0,
+                        fg_color="#FFF",
+                        code=product.get_code(),
+                        desc=product.get_descritpion(),
+                        emb=product.get_emb(),
+                        price=product.get_price(),
+                        status=product.get_status(),
+                        local=product.get_local(),
+                        row=i,
+                        checked=True
+                    )
+                self.table_row.grid(row=i+1, column=0, padx=0, pady=0, sticky="nsew")
 
         
-
-
-        
-
 class TableHead(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
@@ -96,64 +94,70 @@ class TableHead(customtkinter.CTkFrame):
         self.tr_7.grid(row=3, column=6, padx=(2, 50), pady=(2, 2), sticky="nsew")
 
 class TableRow(customtkinter.CTkFrame):
-    def __init__(self, master, checked: bool, code: str, desc: str, emb: str, price: str, local: str, status: str, **kwargs):
+    def __init__(self, master, checked: bool, code: str, desc: str, emb: str, price: str, local: str, status: str, row: int, **kwargs):
         super().__init__(master, **kwargs)
 
         self.checkbox = customtkinter.CTkCheckBox(
             self,
-            border_color="#FFF",
+            border_color="#000",
             border_width=1,
             text=None,
             width=20,
             height=20
         )
-        self.checkbox.grid(row=4, column=0, padx=(2, 2), pady=(2, 2), sticky="nsew",)
+        self.checkbox.grid(row=row+4, column=0, padx=(2, 2), pady=(2, 2), sticky="nsew",)
 
         self.td_2 = customtkinter.CTkLabel(
             self,
             text=f"{code}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            width=30,
+            font=("Inter", 10, "bold"),
+            text_color="#000"
         )
-        self.td_2.grid(row=4, column=1, padx=(2, 40), pady=(2, 2), sticky="nsew")
+        self.td_2.grid(row=row+4, column=1, padx=(2, 10), pady=(2, 2), sticky="nsew")
 
         self.td_3 = customtkinter.CTkLabel(
             self,
-            text=f"{desc}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            text=f"{desc[:25]}",
+            width=200,
+            font=("Inter", 10, "bold"),
+            text_color="#000"
         )
-        self.td_3.grid(row=4, column=2, padx=(2, 180), pady=(2, 2), sticky="nsew")
+        self.td_3.grid(row=row+4, column=2, padx=(2, 10), pady=(2, 2), sticky="nsew")
 
         self.td_4 = customtkinter.CTkLabel(
             self,
             text=f"{emb}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            font=("Inter", 10, "bold"),
+            width=100,
+            text_color="#000"
         )
-        self.td_4.grid(row=4, column=3, padx=(2, 80), pady=(2, 2), sticky="nsew")
+        self.td_4.grid(row=row+4, column=3, padx=(2, 10), pady=(2, 2), sticky="nsew")
 
         self.td_5 = customtkinter.CTkLabel(
             self,
-            text=f"{price}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            text=f"R$ {price}".replace('.', ','),
+            width=80,
+            font=("Inter", 10, "bold"),
+            text_color="#000"
         )
-        self.td_5.grid(row=4, column=4, padx=(2, 50), pady=(2, 2), sticky="nsew")
+        self.td_5.grid(row=row+4, column=4, padx=(2, 0), pady=(2, 2), sticky="nsew")
 
         self.td_6 = customtkinter.CTkLabel(
             self,
             text=f"{local}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            width=80,
+            font=("Inter", 10, "bold"),
+            text_color="#000"
         )
-        self.td_6.grid(row=4, column=5, padx=(2, 50), pady=(2, 2), sticky="nsew")
+        self.td_6.grid(row=row+4, column=5, padx=(2, 2), pady=(2, 2), sticky="nsew")
 
         self.td_7 = customtkinter.CTkLabel(
             self,
             text=f"{status}",
-            font=("Inter", 12, "bold"),
-            text_color="#FFF"
+            width=80,
+            font=("Inter", 10, "bold"),
+            text_color="#000"
         )
-        self.td_7.grid(row=4, column=6, padx=(2, 50), pady=(2, 2), sticky="nsew")
+        self.td_7.grid(row=row+4, column=6, padx=(2, 2), pady=(2, 2), sticky="nsew")
 
