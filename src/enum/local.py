04818cbb1c647b18e5682ff3d4382749f26c2dd8
Jottas <j.pedro.mesquita.cst@gmail.com>
2024-11-01 16:11:30 -0300

    Add function to get name of enum by int number

--- src/enum/local.py
+++ src/enum/local.py
@@ -1,8 +1,10 @@
 from enum import IntEnum
 
-
 class Local(IntEnum):
     EXCEL = 1
     SASOI006 = 2
     RASCUNHO = 3
     TAGSELL = 4
+
+def get_local_name(value):
+    return Local(value).name
--- src/enum/status.py
+++ src/enum/status.py
@@ -5,3 +5,7 @@ class Status(IntEnum):
     PENDING = 1
     EDITED = 2
     PRINTED = 3
+
+
+def get_status_name(value):
+    return Status(value).name
