8f86febc8594ccf318ec01faebf6d395e2637a4e
Jottas <j.pedro.mesquita.cst@gmail.com>
2024-11-01 16:00:08 -0300

    Update enums

---
+++ src/enum/local.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+
+
+class Local(IntEnum):
+    EXCEL = 1
+    SASOI006 = 2
+    RASCUNHO = 3
+    TAGSELL = 4
---
+++ src/enum/status.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class Status(IntEnum):
+    PENDING = 1
+    EDITED = 2
+    PRINTED = 3
